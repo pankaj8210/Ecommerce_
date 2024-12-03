fcb86f0f92dbcbe1e6191b0d7e677e90e8eab560
pankaj8210 <pankajkrdev04@gmail.com>
2024-12-03 13:27:30 +0530

    first commit

---
+++ OneSphere/ecommerce_store/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render, redirect
+from .models import Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+from django import forms
+
+# Create your views here.
+def home(request):
+    products = Product.objects.all()
+    return render(request, 'home.html', {'products': products})
+
+
+def about(request):
+    return render(request, 'about.html', {})
+
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("You have successfully logged In!"))
+            return redirect('home')
+        else:
+            messages.success(request, ('There is an error please try again'))
+            return redirect('login')
+    else:
+        return render(request, 'login.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been logged out... Thanks")
+    return redirect('home')
+
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            # Login in User
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("You Have Successfully Registered Welcome!!"))
+            return redirect('home')
+        else:
+            messages.success(request, ("Please fill in the form correctly"))
+            return redirect('register')
+    else:
+        return render(request, 'register.html', {'form':form})
